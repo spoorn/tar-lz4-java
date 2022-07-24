9234b51a356b428f9399ed523ded77dc0c71479b
spoorn <spookump@gmail.com>
2022-07-24 16:37:26 -0700

    Update build.gradle to add some attributes to MANIFEST and change dependencies to api

--- tar-lz4-java/src/main/java/module-info.java
+++ tar-lz4-java/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module org.spoorn.tarlz4java {
-    requires static org.lz4.java;
-    requires static org.apache.commons.compress;
+    requires org.lz4.java;
+    requires org.apache.commons.compress;
     requires static lombok;
-    requires static org.apache.logging.log4j;
-    requires static org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
     exports org.spoorn.tarlz4java.api;
     exports org.spoorn.tarlz4java.logging;
     exports org.spoorn.tarlz4java.util.concurrent;
