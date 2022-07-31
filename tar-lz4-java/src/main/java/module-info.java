fac75d59140628d1f2ee5d110fe7aac913a40f7b
spoorn <spookump@gmail.com>
2022-07-31 10:43:13 -0700

    Revert "Change log4j to api instead of implementation"
    This reverts commit c7f0ca2f3ffa170afbb2c5283db451781588fcbb.

--- tar-lz4-java/src/main/java/module-info.java
+++ tar-lz4-java/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.spoorn.tarlz4java {
     requires org.lz4.java;
     requires org.apache.commons.compress;
     requires static lombok;
-    requires transitive org.apache.logging.log4j;
+    requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
     exports org.spoorn.tarlz4java.api;
     exports org.spoorn.tarlz4java.logging;
